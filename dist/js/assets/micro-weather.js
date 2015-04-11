26c2b27a4ebeb8cb2fb50fc8888db1a6c948c88d
Phillip B. Kelly <phillipbentonkelly@gmail.com>
2015-04-11 16:42:53 -0400

    Adding new js app wrapper.

---
+++ dist/js/assets/micro-weather.js
@@ -0,0 +1,22 @@
+var MicroWeather = {};
+var microWeatherObj = {};
+
+(function( win, $, undefined){
+	'use strict';
+
+	MicroWeather = function(){
+		if( ! (this instanceof MicroWeather))
+			return new MicroWeather();
+	};
+
+	var _data = {};
+
+	MicroWeather.prototype = {
+		eventHandlers: function(){
+			
+		},
+		initThisObj: function(){
+			
+		}
+	};
+})(window, jQuery);
